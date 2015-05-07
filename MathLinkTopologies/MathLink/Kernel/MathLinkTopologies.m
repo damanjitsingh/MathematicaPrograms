dd6a8da29300b2a6b884228ac07ed01663b12328
Damanjit Singh <damanjits@wolfram.com>
2015-05-07 15:36:52 -0500

    Implemented package for star topology

--- MathLinkTopologies/MathLink/Kernel/MathLinkTopologies.m
+++ MathLinkTopologies/MathLink/Kernel/MathLinkTopologies.m
@@ -3,27 +3,62 @@
 BeginPackage["MathLinkTopologies`"]
 (* Exported symbols added here with SymbolName::usage *) 
 
-StarTopology::usage  = "StarTopology has one master node and four slave nodes connected to the master node"
+StarTopology::usage = "StarTopology[n] consists of one master node and n slave nodes connected to the master node"
+LineTopology::usage = "LineTopology[n] consists of n nodes connected in series"
 
 Begin["`Private`"] (* Begin Private Context *)
 
-links = Table[LinkLaunch["Path to WolframKernel.exe"],{4}];
-(*Writing to each link*)
-Map[LinkWrite[#,"sending hey from master"]&,links];
+StarTopology[slaveCount_]:= Module[{mylinks},
+	(
+		mylinks = Table[LinkLaunch[First[$CommandLine] <> " -wstp"],{slaveCount}];
+		
+		(*Writing to each link*)
+		Map[LinkWrite[#,"sending hey from master"]&,mylinks];
+		Pause[3];
+		
+		Map[ReceiveData[#]&,mylinks];
+	    
+	    Print["Finally close all the opened links"];
+	  	Map[LinkClose[#]&,mylinks]
+  	)
+]
 
-(*Now read the mesage form each link*)
-While[LinkReadyQ[links[[1]]] == True,
-    Print[LinkRead[links[[1]]]] ];
-    
-While[LinkReadyQ[links[[2]]] == True,
-    Print[LinkRead[links[[2]]]] ];
+LineTopology[nodeCount_]:= Module[{link},
+	(
+		link = LinkLaunch[First[$CommandLine] <> " -wstp"];
+		LinkWrite[link,Unevaluated@EvaluatePacket[SendSerialData[2,nodeCount]]];
+		
+		(*Send data from node 1*)
+		LinkWrite[link,"Data from source"];
+	)
+]
 
-While[LinkReadyQ[links[[3]]] == True,
-    Print[LinkRead[links[[3]]]] ];
-    
-While[LinkReadyQ[links[[4]]] == True,
-    Print[LinkRead[links[[4]]]] ];
+(*Auxiliary function*)
+ReceiveData[link_] := Module[{},
+	(
+		While[LinkReadyQ[link] == True,
+		    Print[LinkRead[link]]]
+	)
+]
+
+(*Auxiliary function*)
+SendSerialData[currentNode_,totalNodes_] := Module[{link},
+	(
+		if[currentNodes < totalNodes,
+			link =  LinkLaunch[First[$CommandLine]<> " -wstp"];
+			(*Create a forward link*)
+			LinkWrite[link,Evaluate[SendSerialData[currentNode+1,totalNodes]]];
+			While[True,
+			If[LinkReadyQ[link] == True, 
+				s = LinkRead[link];
+				If[s == LinkClose[]]
+				 ,Null];
+				
+			]
+			,Null
+		 ]
+	)
+]
 
 End[] (* End Private Context *)
-
 EndPackage[]
